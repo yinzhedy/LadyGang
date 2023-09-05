a11a6e1c7534d188c310300f43dd9c12d968c15c
yinzhedy <yinzhe.dy@gmail.com>
2023-09-05 16:30:38 -0400

    created functions to handle changes of create room form and update the state accordingly, now handleRoomButtonPressed can recieve the updated form settings when pressed.

--- LadyGang/frontend/src/components/CreateRoomPage.js
+++ LadyGang/frontend/src/components/CreateRoomPage.js
@@ -15,16 +15,43 @@ export default class CreateRoomPage extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes
+        };
+        
+        this.handleRoomButtomPressed = this.handleRoomButtomPressed.bind(this);
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+        this.handleVotesChange = this.handleVotesChange.bind(this);
+    };
+
+    handleVotesChange(e) {
+        this.setState({
+            votesToSkip: e.target.value
+
+        });
+    };
+
+    handleGuestCanPauseChange(e) {
+        this.setState({
+            guestCanPause: e.target.value === 'true' ? true : false
+        });
+    };
+
+    handleRoomButtomPressed() {
+        console.log(this.state)
     }
 
     render() {
         return(
                 <Grid container spacing={1}>
+
                     <Grid item xs={12} align='center'>
                         <Typography component='h4' variant='h4'>
                             Create A Room
                         </Typography>
                     </Grid>
+
                     <Grid item xs={12} align='center'>
                         <FormControl component='fieldset'>
                             <FormHelperText>
@@ -32,7 +59,7 @@ export default class CreateRoomPage extends Component {
                                     Guest Control of Playback State
                                 </div>
                             </FormHelperText>
-                            <RadioGroup row defaultValue='true'>
+                            <RadioGroup row defaultValue='true' onChange={this.handleGuestCanPauseChange}>
                                 <FormControlLabel 
                                 value='true' 
                                 control={<Radio color='primary'/>}
@@ -48,11 +75,13 @@ export default class CreateRoomPage extends Component {
                             </RadioGroup>
                         </FormControl>
                     </Grid>
+
                     <Grid item xs={12} align='center'>
                         <FormControl>
                             <TextField
                                 required={true}
                                  type='number'
+                                 onChange={this.handleVotesChange}
                                   defaultValue={this.defaultVotes}
                                   inputProps={{
                                     min: 1,
@@ -66,7 +95,7 @@ export default class CreateRoomPage extends Component {
                             </FormHelperText>
                         </FormControl>
                         <Grid item xs={12} align='center'>
-                            <Button color='primary' variant='contained'>
+                            <Button color='primary' variant='contained' onClick={this.handleRoomButtomPressed}>
                                 Create A Room
                             </Button>
                         </Grid>
