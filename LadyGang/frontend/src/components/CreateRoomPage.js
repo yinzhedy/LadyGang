7ec5f241601d3a9fbb1167a580695d49caeff397
yinzhedy <yinzhe.dy@gmail.com>
2023-09-05 16:11:15 -0400

    npm installed mui/material and icons, created basic ui layout for create room page using material components and react link

--- LadyGang/frontend/src/components/CreateRoomPage.js
+++ LadyGang/frontend/src/components/CreateRoomPage.js
@@ -1,13 +1,82 @@
 import React, { Component } from 'react';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import {Link} from 'react-router-dom';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel'
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
+
     constructor(props) {
         super(props);
     }
 
     render() {
         return(
-                <p>CreateRoomPage</p>
+                <Grid container spacing={1}>
+                    <Grid item xs={12} align='center'>
+                        <Typography component='h4' variant='h4'>
+                            Create A Room
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} align='center'>
+                        <FormControl component='fieldset'>
+                            <FormHelperText>
+                                <div align='center'>
+                                    Guest Control of Playback State
+                                </div>
+                            </FormHelperText>
+                            <RadioGroup row defaultValue='true'>
+                                <FormControlLabel 
+                                value='true' 
+                                control={<Radio color='primary'/>}
+                                label='Play/Pause'
+                                labelPlacement = 'bottom'
+                                />
+                                <FormControlLabel 
+                                value='false' 
+                                control={<Radio color='secondary'/>}
+                                label='No Control'
+                                labelPlacement = 'bottom'
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12} align='center'>
+                        <FormControl>
+                            <TextField
+                                required={true}
+                                 type='number'
+                                  defaultValue={this.defaultVotes}
+                                  inputProps={{
+                                    min: 1,
+                                    stlye: {textAlign : 'center'}
+                                }}
+                            />
+                            <FormHelperText>
+                                <div align='center'>
+                                    Votes Required To Skip Song
+                                </div>
+                            </FormHelperText>
+                        </FormControl>
+                        <Grid item xs={12} align='center'>
+                            <Button color='primary' variant='contained'>
+                                Create A Room
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} align='center'>
+                            <Button color='secondary' variant='contained' to='/' component={Link}>
+                                Back
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
         );
     }
 }
