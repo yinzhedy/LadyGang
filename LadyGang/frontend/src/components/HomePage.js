ff0ea240bcac5d4e2c1c10f16539f407c8661b28
yinzhedy <yinzhe.dy@gmail.com>
2023-09-07 20:38:33 -0400

    turned homepage into a functional component instead of class, added buttons to navigate to either creating or joining a room (pages)

--- LadyGang/frontend/src/components/HomePage.js
+++ LadyGang/frontend/src/components/HomePage.js
@@ -1,15 +1,35 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Grid, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-export default class HomePage extends Component{
-    constructor(props){
-        super(props);
-        // this.roomCode = this.props.match.params.roomCode
-        // this.logRoomCode = this.logRoomCode.bind(this)
-    }
-    
-    render() {
-        return <div>
-            HomePage
-        </div>
-    }
-}
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    const joinRoomButtonPressed = () => {
+        navigate('/join'); // Navigate to the '/join' route
+    };
+
+    const createRoomButtonPressed = () => {
+        navigate('/create'); // Navigate to the '/create' route
+    };
+
+    return (
+        <Grid container>
+            <Grid item xs={12} align='center'>
+                <h3>HomePage</h3>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <Button variant='contained' color='primary' onClick={createRoomButtonPressed}>
+                    Create A Room
+                </Button>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <Button variant='contained' color='secondary' onClick={joinRoomButtonPressed}>
+                    Join A Room
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default HomePage;
